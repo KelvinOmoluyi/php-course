b09c711d806e35891d2498c00ddc852693de2ee7
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-22 23:15:07 +0100

    intialized

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'demo',
+        'username' => 'root',
+        'password' => 'kelvincoding',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];
