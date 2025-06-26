01d9ca07df4c783816629b833196f54817f4fddd
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-26 22:54:11 +0100

    initialized

--- public/index.php
+++ public/index.php
@@ -1,25 +1,36 @@
 <?php
 
-session_start();
+use Core\Session;
+use Core\ValidationException;
 
 const BASE_PATH = __DIR__.'/../';
 
-require BASE_PATH.'Core/functions.php';
+require BASE_PATH . '/vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    // Core database
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);    // overwrote class
-    require base_path("{$class}.php");
-});
+session_start();
+
+require BASE_PATH.'Core/functions.php';
 
 require base_path('bootstrap.php');
 
 $router = new \Core\Router(); // initializing
 
-$routes = require(base_path('routes.php'));
+$routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+try {
 
+    $router->route($uri, $method);
+
+} catch (ValidationException $exception){
+
+    Session::flash('errors', $exception->errors);
+
+    Session::flash('old', $exception->old);
+    redirect('/login');
+}
+
+// unflash session
+Session::unflash();
 
